5903b2b856c467101c68e53814385325330cb180
ZKYRIO <hamdanioke1512@gmail.com>
2023-09-11 21:47:25 +0700

    Menambahkan unlink untuk menghapus foto user dari folder saat delete user

--- admin/user_delete.php
+++ admin/user_delete.php
@@ -6,11 +6,16 @@ $id = $_GET['id_user'];
 // ambil data user dari database
 $users = mysqli_query($db, "SELECT * FROM user WHERE id = $id");
 $user = mysqli_fetch_assoc($users);
+$foto_user_lama = $user["foto_user"];
 
 // sql delete query
 mysqli_query($db, "DELETE FROM user WHERE id = $id ");
 
 if ( mysqli_affected_rows($db) > 0 ) {
+
+    // hapus foto user dari file
+    unlink("./img/users/$foto_user_lama");
+
     echo "
     <script>
         alert('Data Berhasil Di Hapus!')
