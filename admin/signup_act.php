6ab97d9d8ffe1af497f853f5c5b1514f90558e9f
ZKYRIO <hamdanioke1512@gmail.com>
2023-09-12 21:41:52 +0700

    Menambahkan syntax untuk upload foto dan function upload file

--- admin/signup_act.php
+++ admin/signup_act.php
@@ -34,10 +34,67 @@ function signup($data) {
     // Enskripsi password
     $password = password_hash($password, PASSWORD_DEFAULT);
 
+    // Cek apakah user meng upload foto profile atau tidak
+    if ( $_FILES['foto_user']['error'] === 4) {
+        $foto_user = $profDefault;
+    }else {
+        $fotoFor = "foto_user";
+        $foto_user = upload($fotoFor);
+    }
+
     // Insert usn & pass to database
-    mysqli_query($db, "INSERT INTO user VALUES( NULL ,'$nama', '$email' , '$password' , '$profDefault' )");
+    mysqli_query($db, "INSERT INTO user VALUES( NULL ,'$nama', '$email' , '$password' , '$foto_user' )");
 
     return mysqli_affected_rows($db);
+
+}
+
+// Function untuk upload file
+function upload($fotoFor) {
+    $namaFile = $_FILES["$fotoFor"]['name'];
+    $ukuranFile = $_FILES["$fotoFor"]["size"];
+    $error = $_FILES["$fotoFor"]["error"]; 
+    $tmp = $_FILES["$fotoFor"]["tmp_name"];
+
+    //  cek apakh gambar diupload atau tidak
+    if($error === 4) {
+        echo "
+            <script>
+                alert('pilih gambar dahulu!');
+            </script>
+        ";
+        return false;
+    }
+
+    //  cek apakah yang diupload adalah gambar
+    $ekstensiGambarValid = ['jpg','jpeg','png'];
+    $ekstensiGambar = pathinfo($namaFile, PATHINFO_EXTENSION);
+    if( in_array( $ekstensiGambar, $ekstensiGambarValid ) === false) {
+        echo "
+            <script>
+                alert('File gambar {$fotoFor} yang anda upload bukan format gambar!');
+            </script>
+        ";
+        return false;
+    }
+
+    // cek jika ukuran nya terlalu besar
+    if( $ukuranFile > 10000000 ) {
+        echo "
+            <script>
+                alert('ukuran gambar {$fotoFor} melebihi 10mb!');
+            </script>
+        ";
+        return false;
+    }
+
+    // gambar siap diupload
+    $namaFileBaru = uniqid() ;
+    $namaFileBaru .= '.' ;
+    $namaFileBaru .= $ekstensiGambar;
+    move_uploaded_file($tmp , './img/users/' . $namaFileBaru);
+
+    return $namaFileBaru;
 
 }
 ?>
